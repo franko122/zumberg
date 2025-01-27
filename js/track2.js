6d2c9e2cf3e4db2aeec10d984f7d37cb6da64e57
franko122 <83500527+franko122@users.noreply.github.com>
2025-01-27 18:36:49 +0100

    Update track2.js

--- js/track2.js
+++ js/track2.js
@@ -1,9 +1,42 @@
+function StartWork() {
+    let bag = document.getElementById('bag')
+    let neto = document.getElementById("neto");
+    let loki  = document.getElementById('loki')
+    let lokij = document.getElementById('lokij');
+    let menu = document.getElementById('menu');
+    let secmenu = document.getElementById('secmenu');
+    let lou = document.getElementById('lou');
+    let ouns = document.getElementById('ouns')
+    
+   let track1 = 6 ; 
 
-function  startTrack() {
-  let neto = document.getElementById("neto") 
-  neto.addEventListener("click", ()=>{
-    alert("done")
+   bag.addEventListener('click' ,()=>{
+          bag.style.border='solid  rgb(2, 62, 127) 4px';
+          ouns.style.display="none"
+   }) 
+   
+  setInterval(() => {
+    lokij.style.display="none"
+  }, 3000);
+
+   neto.addEventListener('click',()=>{ 
+        if (bag.value == track1) { 
+            loki.style.display="flex"
+            setTimeout(() => {
+              window.location="main2.html"
+            }, 3000);
+          }
+          
+        else{
+          bag.style.border='solid red 3px'  
+          ouns.style.display="block"
+        } 
   })
-
+  menu.addEventListener('click',()=>{ 
+    secmenu.style.width="100%";
+  }) 
+  lou.addEventListener('click',()=>{ 
+    secmenu.style.width="0%"
+  }) 
 }
-startTrack()
+StartWork()
