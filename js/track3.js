c18356ff1700a630d1283cf529c2f4cc3e12e9aa
franko122 <83500527+franko122@users.noreply.github.com>
2025-01-27 20:01:49 +0100

    Update track3.js

--- js/track3.js
+++ js/track3.js
@@ -8,7 +8,7 @@ function StartWork() {
     let lou = document.getElementById('lou');
     let ouns = document.getElementById('ouns')
     
-   let track1 = 6636374774333; 
+   let track1 = 6; 
 
    bag.addEventListener('click' ,()=>{
           bag.style.border='solid  rgb(2, 62, 127) 4px';
