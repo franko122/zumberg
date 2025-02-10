82542842fd90f6ffce9425ab3ee1ec8e32d57b15
franko122 <83500527+franko122@users.noreply.github.com>
2025-02-10 09:07:52 +0100

    Update track5.js

--- js/track5.js
+++ js/track5.js
@@ -1,1 +1,43 @@
 
+function StartWork() {
+    let bag = document.getElementById('bag')
+    let neto = document.getElementById("neto");
+    let loki  = document.getElementById('loki')
+    let lokij = document.getElementById('lokij');
+    let menu = document.getElementById('menu');
+    let secmenu = document.getElementById('secmenu');
+    let lou = document.getElementById('lou');
+    let ouns = document.getElementById('ouns')
+    
+   let track1 =737773637383837;
+
+   bag.addEventListener('click' ,()=>{
+          bag.style.border='solid  rgb(2, 62, 127) 4px';
+          ouns.style.display="none"
+   }) 
+   
+  setInterval(() => {
+    lokij.style.display="none"
+  }, 3000);
+
+   neto.addEventListener('click',()=>{ 
+        if (bag.value == track1) { 
+            loki.style.display="flex"
+            setTimeout(() => {
+              window.location="main5.html"
+            }, 3000);
+          }
+          
+        else{
+          bag.style.border='solid red 3px'  
+          ouns.style.display="block"
+        } 
+  })
+  menu.addEventListener('click',()=>{ 
+    secmenu.style.width="100%";
+  }) 
+  lou.addEventListener('click',()=>{ 
+    secmenu.style.width="0%"
+  }) 
+}
+StartWork()
